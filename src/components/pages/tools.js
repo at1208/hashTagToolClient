ea1aed34ad3d672e1846450197f5b4ab7e4e4004
at1208 <amantiwari12081999@gmail.com>
2019-10-01 18:09:05 +0530

    something added

--- src/components/pages/tools.js
+++ src/components/pages/tools.js
@@ -1,9 +1,24 @@
 import React from 'react';
-
+import './tool.css'
 const Tools = () => {
-  return <>
+  return <div className='container b3'>
     <h1 className='text-center'> Tools </h1>
-  </>
+
+    <div className='container'>
+    <label className='container shadow b1'>Input 1</label>
+    <input
+    />
+    </div>
+
+    <div className='container'>
+    <label className='container shadow b1'>Input 2</label>
+    <input
+    />
+    <br />
+     <button className='b2  btn-sm'> Submit</button>
+    </div>
+
+  </div>
 }
 
 export default Tools;
