cc3104937d72435effc1d2c8687abc3741593976
at1208 <amantiwari12081999@gmail.com>
2019-10-07 22:16:02 +0530

    added react-autosuggestion

--- src/components/pages/tools.js
+++ src/components/pages/tools.js
@@ -1,51 +1,161 @@
 import React, { Component } from 'react';
 import './tools.css'
-import axios from 'axios'
+import axios from 'axios';
 import Slide from 'react-reveal/Slide';
 import Zoom from 'react-reveal/Zoom';
 import Fade from 'react-reveal/Fade';
+import Autosuggest from 'react-autosuggest';
+
+const Niche = [
+  {
+    name: 'Jeans',
+
+  },
+  {
+    name: 'Leather_Jacket',
+
+  },
+  {
+    name: 'Leather_Pants',
+
+  },
+  {
+    name: 'Leather_Clothing',
+
+  },
+  {
+    name: 'Mens_and_Boys_Hats',
+
+  },
+  {
+    name: 'Mens_Suits',
+
+  },
+  {
+    name: 'Neckwear',
+
+  },
+  {
+    name: 'Pants',
+
+  },
+  {
+    name: 'Shirts',
+
+  },
+  {
+    name: 'Surplus_and_Salvage_Merchandise',
+
+  },
+
+
+];
+
+const getSuggestions = value => {
+  const inputValue = value.trim().toLowerCase();
+  const inputLength = inputValue.length;
+
+  return inputLength === 0 ? [] : Niche.filter(niche =>
+    niche.name.toLowerCase().slice(0, inputLength) === inputValue
+  );
+};
+
+const getSuggestionValue = suggestion => suggestion.name;
+
+const renderSuggestion = suggestion => (
+  <div className='c1 shadow'>
+   {suggestion.name}
+  </div>
+);
 
 class Tools extends Component {
 
  state = {
    data: [],
-   niche:''
+
+
+   value: '',
+   suggestions: []
  }
+
+ onChange = (event, { newValue }) => {
+   this.setState({
+     value: newValue
+   });
+ };
+
+ onSuggestionsFetchRequested = ({ value }) => {
+    this.setState({
+      suggestions: getSuggestions(value)
+    });
+  };
+
+  onSuggestionsClearRequested = () => {
+    this.setState({
+      suggestions: []
+    });
+  };
 
 
 
   onSubmitNiche = () => {
-    const niche = this.state.niche
+
+    const niche = this.state.value
     axios.get(`https://cors-anywhere.herokuapp.com/https://hashhy.herokuapp.com/${niche}`)
     .then((data) => this.setState({ data: data.data}))
     .catch(err => console.log(err))
   }
 
   render(){
+console.log(this.state.value)
+    const { value, suggestions } = this.state;
+
+    const inputProps = {
+        placeholder: 'Search for niche',
+        value,
+        onChange: this.onChange
+      };
+
     console.log(this.state.data)
     return <div className='container'>
             <Slide top>
     <h1 className='text-center'>Tool</h1>
             </Slide>
 
-<div className='container text-center b13'>
- <Zoom top>
-      <div className='b4 text-center container'>
-          <div className='container b2 float-left '>
-         <input className='b3 text-center' placeholder='Search for niche' onChange = {(e) => this.setState({ niche: e.target.value})} value = {this.state.niche}/>
-         </div>
-      </div>
-          </Zoom>
+<div className='container-fluid row-sm-6 c2'>
 
-<br />
- <Zoom top>
-      <div className='b6 container'>
-      <button className='b5 btn-lg' onClick = {this.onSubmitNiche}>SEARCH</button>
-      </div>
-        </Zoom>
+        <div className='container text-center b13 row'>
+<Zoom top>
 
+            <div className='b4 text-center container'>
+
+                <div className='container b2 float-left'>
+
+
+                        <Autosuggest
+                     suggestions={suggestions}
+                     onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+                     onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+                     getSuggestionValue={getSuggestionValue}
+                     renderSuggestion={renderSuggestion}
+                     inputProps={inputProps}
+                   />
+
+               </div>
+
+           </div>
+
+</Zoom>
+
+<Zoom top>
+            <div className='b6 container'>
+            <button className='b5 btn-lg' onClick = {this.onSubmitNiche}>Find Hashtag</button>
+            </div>
+
+</Zoom>
 </div>
 
+</div>
 
 
  <div className='container b7 text-center'>
