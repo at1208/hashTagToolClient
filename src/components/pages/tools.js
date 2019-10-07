d95d17e3b36fcc1dcb02b9cb34147832101f0f5e
at1208 <amantiwari12081999@gmail.com>
2019-10-07 06:56:23 +0530

    input and search button added

--- src/components/pages/tools.js
+++ src/components/pages/tools.js
@@ -6,6 +6,19 @@ class Tools extends Component {
   render(){
     return <div className='container'>
     <h1 className='text-center'>Tool</h1>
+
+<div className='container text-center'>
+      <div className='b4 text-center container'>
+          <div className='container b2 float-left '>
+         <input className='b3 text-center' placeholder='Search for niche'/>
+         </div>
+      </div>
+
+<br />
+      <div className='b6 container'>
+      <button className='b5 btn-lg'>SEARCH</button>
+      </div>
+</div>
     </div>
   }
 }
